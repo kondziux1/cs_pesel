6248ca2ba80324db7046b834f5f60ec7537c19af
kondziux1 <konrad.frukacz@op.pl>
2024-10-04 15:42:51 +0200

    sprawdzanie poprawności

--- Program.cs
+++ Program.cs
@@ -17,5 +17,42 @@
                 return 'M';
             }
         }
+        static bool checkParrity(int[] elvdigit)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 }; //10
+            int[] corrector = new int[9];
+            int S = 0, M = 0, R = 0;
+
+            int check = elvdigit[10];
+            //dohickey set a corrector with iloczyn
+            for (int i = 0; i < 9; i++)
+            {
+                corrector[i] = weights[i] * elvdigit[i];
+            }
+            //sumuj
+            for (int i = 0; i < 9; i++)
+            {
+                S += corrector[i];
+            }
+            //modulo
+            M = S % 10;
+            //dohiki
+            if (M == 0)
+            {
+                R = 0;
+            }
+            else
+            {
+                R = 10 - M;
+            }
+            //final check
+            if (R == check)
+            {
+
+                return true;
+            }
+
+            return false;
+        }
     }
 }
